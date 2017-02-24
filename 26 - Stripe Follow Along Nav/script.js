257d04f56b28fe6861101661af8584d8f72bb8d8
Larisa Moroz <larisa.moroz@gmail.com>
2017-02-25 02:33:28 +0800

    Follow Along Nav little better

--- 26 - Stripe Follow Along Nav/script.js
+++ 26 - Stripe Follow Along Nav/script.js
@@ -1,21 +1,20 @@
 const navLinks = document.querySelectorAll('nav ul.cool>li');
 const navPopup = document.querySelector('div.dropdownBackground');
-const navOffset = (document.querySelector('nav ul.cool').getBoundingClientRect()).top;
 
 function showDropdown() {
     this.classList.add('trigger-enter');
     navPopup.classList.add('open');
     const {width, height, top, left} = this.querySelector('.dropdown').getBoundingClientRect();
+    const navOffset = (document.querySelector('nav ul.cool').getBoundingClientRect()).top;
 
-    navPopup.style.width = `${width}px`;
-    navPopup.style.height = `${height}px`;
-    navPopup.style.transform = `translate(${left}px, ${top - navOffset}px)`;
+    navPopup.style.setProperty('width', `${width}px`);
+    navPopup.style.setProperty('height', `${height}px`);
+    navPopup.style.setProperty('transform', `translate(${left}px, ${top - navOffset}px)`);
 
-    window.setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    window.setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 200);
 }
 function hideDropdown() {
-    this.classList.remove('trigger-enter-active');
-    this.classList.remove('trigger-enter');
+    this.classList.remove('trigger-enter-active', 'trigger-enter');
     navPopup.classList.remove('open');
 }
 
